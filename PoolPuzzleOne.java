b2ff681f8bdd4e13a8de322e21be5ab995964f1e
RaymondXiao1991@hotmail.com <RaymondXiao1991@hotmail.com>
2018-07-04 19:08:20 +0800

    First commit

---
+++ PoolPuzzleOne.java
@@ -0,0 +1,50 @@
+public class PoolPuzzleOne {
+    public static void main(String [] args) {
+        int x = 0;
+        while (x <4 ) {
+            System.out.print("a ");
+            System.out.print("n ");
+            System.out.print("an");
+
+            x = x + 2;
+            x = x - 2;
+            x = x - 1;
+            System.out.print(" ");
+
+            x > 1
+            x > 3
+            x < 4
+            System.out.print("noys ");
+            System.out.print("oise ");
+            System.out.print(" oyster ");
+            System.out.print("annoys");
+            System.out.print("noise");
+            if ( x < 1 ) {
+                ___________________________
+            }
+            _____________________________
+            if (   x > 1 ) {
+
+                ____________________________
+
+                        ___________
+            }
+            if ( x == 1 ) {
+                ____________________________
+            }
+            if ( ___________ ) {
+                ____________________________
+            }
+            System.out.println("");
+            x = x + 1;
+        }
+    }
+}
+
+
+/*
+%java PoolPuzzleOne
+a noise
+annoys
+an oyster
+ */
